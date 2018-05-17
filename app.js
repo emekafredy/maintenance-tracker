e174cd9c4061dc10c2aa7b5cc93542f99408605f
emekafredy <chinedufrderick@gmail.com>
2018-05-17 22:35:47 +0100

    [ft #157649073] Setup api endpoint to get all requests

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import winston from 'winston';
 
+import router from './server/routes';
+
 
 // Set up the express app
 const app = express();
@@ -10,8 +12,8 @@ const port = process.env.PORT || 4500;
 // Parse incoming requests data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('UI'));
 
+router(app);
 
 app.listen(port);
 winston.log('info', `App is listening on port ${port}`);

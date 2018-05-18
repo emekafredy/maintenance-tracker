03afe9a0dab3a16e52c3d1441ca6f882c2d55f30
emekafredy <chinedufrderick@gmail.com>
2018-05-18 03:38:16 +0100

    [ft #157561728] Improve the User Interface

--- UI/js/users.js
+++ UI/js/users.js
@@ -1,34 +1,37 @@
-let submitRequest = document.getElementById('submitRequest');
 
-submitRequest.addEventListener('click', () => {
-  modal.createMessage();
+
+let removeRequest = document.getElementById('removeRequest');
+removeRequest.addEventListener('click', () => {
+  deleteRequest();
 })
 
+const deleteRequest = () => {
+  let modalDiv = document.getElementById('modal-container');
+  modalDiv.innerHTML = `
+      <div class="modal-div">
+        <div> <i class="fa fa-check-circle"></i> </div>
+        <p id="messageId">Request Successfully deleted</p>
+        <button id="close">close</button>
+      </div>
+  `
+  let message = confirm("Do you really want to delete this request?");
 
-const modal = {
-
-  createMessage: () => {
-    let modalDiv = document.getElementById('modal-container');
-    modalDiv.innerHTML = `
-        <div class="modal-div">
-          <div> <i class="fa fa-check-circle"></i> </div>
-          <p id="messageId">Request Successfully Created</p>
-          <button id="close">close</button>
-        </div>
-    `
-    modalDiv.style.display = "block";
-
+  if (message === true) {
+    return modalDiv.style.display = "block";
     window.onclick = function(event) {
       if (event.target == modalDiv) {
         modalDiv.style.display = "none";
       }
     } 
-
-
-    let closeBtn = document.getElementById("close");
+  
+  
+    var closeBtn = document.getElementById("close");
     closeBtn.onclick = function() {
       modalDiv.style.display = "none";
     }
   }
+  return null;
+  
+}
 
-};
+

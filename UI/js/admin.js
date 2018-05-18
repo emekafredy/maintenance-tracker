48a0323013945949f9bdd9674e8a40335a254fcc
emekafredy <chinedufrderick@gmail.com>
2018-05-18 22:21:04 +0100

    [ft #157561728] Modify the UI to have better interaction and design

--- UI/js/admin.js
+++ UI/js/admin.js
@@ -33,10 +33,11 @@ const process = {
   },
   rejectRequest: () => {
     if (requestStatus.textContent == 'Pending') {
-      requestStatus.textContent = 'Rejected';
-      approveBtn.style.display = 'none';
-      rejectBtn.style.display = 'none';
-      resolveBtn.style.display = 'none';
+        rejectARequest();
+        requestStatus.textContent = 'Rejected';
+        approveBtn.style.display = 'none';
+        rejectBtn.style.display = 'none';
+        resolveBtn.style.display = 'none';  
     }
   },
   resolveRequest: () => {
@@ -48,5 +49,40 @@ const process = {
       resolveHeader.style.display = 'block';
       resolveDate.textContent = formatDate(new Date());
     }
+  }
+}
+
+
+
+let modalDiv = document.getElementById('modal-container');
+const rejectARequest = () => {
+  modalDiv.innerHTML = `
+      <div class="modal-div">
+        <p id="messageId">Do you really want to reject this request?</p>
+        <button id="proceed" onclick="proceedWithRejection();">Proceed</button>
+        <button id="close">Cancel</button>
+      </div>
+  `
+  modalDiv.style.display = "block";
+
+  let closeBtn = document.getElementById("close");
+  closeBtn.onclick = function() {
+    modalDiv.style.display = "none";
+  }
+}
+
+const proceedWithRejection = () => {
+  modalDiv.innerHTML = `
+      <div class="modal-div">
+        <div> <i class="fa fa-check-circle"></i> </div>
+        <p id="messageId">Request marked as rejected</p>
+        <button id="closeAfterdelete">close</button>
+      </div>
+  `
+  modalDiv.style.display = "block";
+
+  let closeBtn = document.getElementById("closeAfterdelete");
+  closeBtn.onclick = function() {
+    modalDiv.style.display = "none";
   }
 }

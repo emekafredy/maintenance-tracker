6cb12b4e312b6faf3c438a2f76fae1e0497108bc
emekafredy <chinedufrderick@gmail.com>
2018-05-25 01:59:48 +0100

    [ft #157730982] Create endpoint for user to update their requests

--- server/controllers/userRequestsController.js
+++ server/controllers/userRequestsController.js
@@ -14,7 +14,7 @@ class UserRequestsController {
       return response.status(204).json({
         message: 'You have no requests record',
       });
-    }).catch(error => next(error));
+    }).catch(err => next(err.message));
   }
 
   static getSingleRequest(request, response, next) {
@@ -33,7 +33,7 @@ class UserRequestsController {
             data: data.rows,
           });
       })
-      .catch(err => next(err));
+      .catch(err => next(err.message));
     if (Number.isNaN(reqId)) {
       return response.status(400).json({
         message: 'Your request is invalid. Please enter a number',
@@ -64,8 +64,52 @@ class UserRequestsController {
     client.query(query).then(myRequest => response.status(201).json({
       message: 'Request Successfully created',
       newRequest,
-    })).catch(error => next(error));
+    })).catch(err => next(err.message));
 
+    return null;
+  }
+
+  static updateRequest(request, response, next) {
+    const reqId = parseInt(request.params.requestId, 10);
+    const { userid: userId } = request.user;
+    const updatedRequest = {
+      product: request.body.product || response.req.body.product,
+      requestType: request.body.requestType || response.req.body.requestType,
+      issue: request.body.issue || response.req.body.issue,
+    };
+    const query = {
+      text: 'UPDATE requests SET product=$1, requestType=$2, issue=$3 WHERE requestId=$4',
+      values: [
+        updatedRequest.product,
+        updatedRequest.requestType,
+        updatedRequest.issue,
+        reqId],
+    };
+    if (RequestValidator.checkUpdate(request, response)) {
+      return null;
+    }
+    client.query('select * from requests where userId = $1 and requestId = $2', [userId, reqId])
+      .then((data) => {
+        if (data.rows.length === 0) {
+          response.status(400).json({
+            message: 'You have no request with this ID',
+          });
+        } else if (data.rows[0].requeststatus !== 'pending') {
+          response.status(401).json({
+            message: 'You can no longer update this request',
+          });
+        }
+        return client.query(query).then(myRequest => response.status(200).json({
+          message: 'Request successfully updated',
+          updatedRequest,
+        })).catch(err => next(err.message));
+      }).catch(err => next(err.message));
+
+    if (Number.isNaN(reqId)) {
+      return response.status(400).json({
+        message: 'Your request is invalid. Please enter a number',
+      });
+    }
     return null;
   }
 }

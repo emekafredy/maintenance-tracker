b6c8c3798b73580a5f5a87c565bc57ffbe673255
emekafredy <chinedufrderick@gmail.com>
2018-05-31 04:05:10 +0100

    Host static file on Heroku

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,24 +1,3 @@
-const getPages = {
-  signUpPage: () => {
-    window.location.href = 'sign-up.html';
-  },
-  signUpUser: () => {
-    window.location.href = 'user-requests.html';
-  },
-  loginUser: () => {
-    window.location.href = 'user-requests.html';
-  },
-  getDetails: () => {
-    window.location.href = 'request-details.html';
-  },
-  backToRequests: () => {
-    window.location.href = 'user-requests.html';
-  },
-  backToAdmin: () => {
-    window.location.href = 'admin-panel.html';
-  },
-};
-
 const processRequests = () => {
   window.location.href = 'process-requests.html';
 };

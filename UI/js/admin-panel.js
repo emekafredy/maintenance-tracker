7cbd76890d7c8a4d172c05208aaee9e20a2563a7
emekafredy <chinedufrderick@gmail.com>
2018-06-04 13:21:50 +0100

    [ft #157980941] Fetch API for logged in Admin to get all requests in the app

--- UI/js/admin-panel.js
+++ UI/js/admin-panel.js
@@ -1,5 +1,4 @@
-const requestsUrl = 'https://emeka-m-tracker.herokuapp.com/api/v1/users/requests';
-
+const allRequestsUrl = 'https://emeka-m-tracker.herokuapp.com/api/v1/requests';
 
 const token = localStorage.getItem('authToken');
 
@@ -11,7 +10,7 @@ const options = {
   },
 };
 
-fetch(requestsUrl, options)
+fetch(allRequestsUrl, options)
   .then(response => response.json())
   .then((json) => {
     const { message, data } = json;
@@ -19,32 +18,30 @@ fetch(requestsUrl, options)
     const requestMessage = document.getElementById('status-message');
     requestMessage.innerHTML = message;
 
-    const table = document.getElementById('requests-table');
+    const table = document.getElementById('users-requests');
     for (let index = 0; index < data.length; index += 1) {
       const row = table.insertRow(index + 1);
 
       const cell1 = row.insertCell(0);
       cell1.setAttribute('data-label', 'Request ID');
       const cell2 = row.insertCell(1);
-      cell2.setAttribute('data-label', 'Product');
+      cell2.setAttribute('data-label', 'User ID');
       const cell3 = row.insertCell(2);
-      cell3.setAttribute('data-label', 'Request Type');
+      cell3.setAttribute('data-label', 'Product');
       const cell4 = row.insertCell(3);
-      cell4.setAttribute('data-label', 'Status');
+      cell4.setAttribute('data-label', 'Request Type');
       const cell5 = row.insertCell(4);
-      cell5.setAttribute('data-label', 'Details');
+      cell5.setAttribute('data-label', 'Status');
       const cell6 = row.insertCell(5);
-      cell6.setAttribute('data-label', 'Cancel');
+      cell6.setAttribute('data-label', 'Details');
 
       cell1.innerHTML = data[index].requestid;
-      cell2.innerHTML = data[index].product;
-      cell3.innerHTML = data[index].requesttype;
-      cell4.innerHTML = data[index].requeststatus;
-      cell5.innerHTML = `<button class="btn btn-details" onclick="checkDetails(${data[index].requestid});"> 
+      cell2.innerHTML = data[index].userid;
+      cell3.innerHTML = data[index].product;
+      cell4.innerHTML = data[index].requesttype;
+      cell5.innerHTML = data[index].requeststatus;
+      cell6.innerHTML = `<button class="btn btn-details" onclick="checkDetails(${data[index].requestid});"> 
                           <i class="fa fa-trash"></i> Details 
-                        </button>`;
-      cell6.innerHTML = `<button class="btn btn-delete"> 
-                          <i class="fa fa-trash"></i> Cancel 
                         </button>`;
     }
   });

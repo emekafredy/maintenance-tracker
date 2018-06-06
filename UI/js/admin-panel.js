56e8a439c0da1c2816b183798bddaa50094a72ea
emekafredy <chinedufrderick@gmail.com>
2018-06-07 00:58:45 +0100

    [ft #157980486] fetch api for logged in users to update a request that has not been processed yet

--- UI/js/admin-panel.js
+++ UI/js/admin-panel.js
@@ -1,4 +1,4 @@
-const allRequestsUrl = 'https://emeka-m-tracker.herokuapp.com/api/v1/requests';
+const allRequestsUrl = 'https://emeka-m-tracker.herokuapp.com/api/v1/requests/';
 
 const token = localStorage.getItem('authToken');
 
@@ -11,6 +11,97 @@ const options = {
 };
 
 /**
+ * @description fetch method to consume API used to get users' request details for logged in admin.
+ *
+ * @param {string} requestsUrl - API endpoint
+ * @param {Object} options - Mthod and headers
+ *
+ * @returns {object} response JSON Object
+ */
+const modalDiv = document.getElementById('details-modal');
+const requestDetailsModal = (data, message) => {
+  const card = document.createElement('div');
+  card.className = 'card';
+
+  const header = document.createElement('div');
+  header.className = 'header';
+  header.innerHTML = `<h1>${message}</h1>`;
+
+  const wrapper = document.createElement('div');
+  wrapper.className = 'wrapper';
+
+  const column1 = document.createElement('div');
+  column1.className = 'column';
+
+  const column2 = document.createElement('div');
+  column2.className = 'column';
+
+  const imgDiv = document.createElement('div');
+  imgDiv.innerHTML = `<img src="${data.imageurl}" />`;
+
+  column1.appendChild(imgDiv);
+
+  const btnDiv = document.createElement('div');
+  btnDiv.className = 'btn-div';
+
+  const approveBtn = document.createElement('button');
+  approveBtn.className = 'btn btn-details';
+  approveBtn.innerHTML = '<i class="fa fa-thumbs-up"></i> Approve';
+
+  const disapproveBtn = document.createElement('button');
+  disapproveBtn.className = 'btn btn-delete';
+  disapproveBtn.innerHTML = '<i class="fa fa-thumbs-down"></i> Disapprove';
+
+  const resolveBtn = document.createElement('button');
+  resolveBtn.className = 'btn resolve-btn';
+  resolveBtn.innerHTML = '<i class="fa fa-check"></i> Check as resolved';
+  column2.innerHTML = `
+              <p> <label>Request ID</label> : <span>${data.requestid}</span></p>
+              <p> <label>User ID</label> : <span>${data.userid}</span></p>
+              <p> <label>Product</label> : <span>${data.product}</span></p>
+              <p> <label>Request Date</label> : <span>${data.requestdate}</span></p>
+              <p> </p><label>Request type</label> : <span>${data.requesttype}</span></p>
+              <p> <label>Isssue</label>: <br>
+              <span> ${data.issue}</span></p>
+              <p> </p><label>Request status</label> : <span>${data.requeststatus}</span></p>
+              <p> </p><label>Approved At</label> : <span>${data.approvedat}</span></p>
+              <p> </p><label>Disapproved At</label> : <span>${data.disapprovedat}</span></p>
+              <p> </p><label>Resolved At</label> : <span>${data.resolvedat}</span></p>
+  `;
+
+  btnDiv.appendChild(approveBtn);
+  btnDiv.appendChild(disapproveBtn);
+  btnDiv.appendChild(resolveBtn);
+  column2.appendChild(btnDiv);
+  card.appendChild(header);
+  card.appendChild(wrapper);
+  wrapper.appendChild(column1);
+  wrapper.appendChild(column2);
+  modalDiv.appendChild(card);
+
+  if (data.requeststatus === 'pending') {
+    resolveBtn.style.display = 'none';
+  }
+  if (data.requeststatus === 'approved') {
+    approveBtn.style.display = 'none';
+    disapproveBtn.style.display = 'none';
+  }
+  if (data.requeststatus === 'disapproved' || data.requeststatus === 'resolved') {
+    approveBtn.style.display = 'none';
+    disapproveBtn.style.display = 'none';
+    resolveBtn.style.display = 'none';
+  }
+  modalDiv.style.display = 'block';
+
+  window.addEventListener('click', (event) => {
+    if (event.target === modalDiv) {
+      modalDiv.style.display = 'none';
+      modalDiv.innerHTML = '';
+    }
+  });
+};
+
+/**
    * @description fetch method to consume API used to get all users' requests for logged in admin.
    *
    * @param {string} requestsUrl - API endpoint
@@ -18,38 +109,51 @@ const options = {
    *
    * @returns {object} response JSON Object
    */
-fetch(allRequestsUrl, options)
-  .then(response => response.json())
-  .then((json) => {
-    const { message, data } = json;
+const fetchAllRequests = () => {
+  fetch(allRequestsUrl, options)
+    .then(response => response.json())
+    .then((json) => {
+      const { message, data } = json;
 
-    const requestMessage = document.getElementById('status-message');
-    requestMessage.innerHTML = message;
+      const requestMessage = document.getElementById('status-message');
+      requestMessage.innerHTML = message;
 
-    const table = document.getElementById('users-requests');
-    for (let index = 0; index < data.length; index += 1) {
-      const row = table.insertRow(index + 1);
+      const table = document.getElementById('users-requests');
+      for (let index = 0; index < data.length; index += 1) {
+        const row = table.insertRow(index + 1);
 
-      const cell1 = row.insertCell(0);
-      cell1.setAttribute('data-label', 'Request ID');
-      const cell2 = row.insertCell(1);
-      cell2.setAttribute('data-label', 'User ID');
-      const cell3 = row.insertCell(2);
-      cell3.setAttribute('data-label', 'Product');
-      const cell4 = row.insertCell(3);
-      cell4.setAttribute('data-label', 'Request Type');
-      const cell5 = row.insertCell(4);
-      cell5.setAttribute('data-label', 'Status');
-      const cell6 = row.insertCell(5);
-      cell6.setAttribute('data-label', 'Details');
+        const detailsBtn = document.createElement('button');
+        detailsBtn.className = 'btn btn-details';
+        detailsBtn.innerHTML = '<i class="fa fa-arrow-circle-o-right"></i> Details';
 
-      cell1.innerHTML = data[index].requestid;
-      cell2.innerHTML = data[index].userid;
-      cell3.innerHTML = data[index].product;
-      cell4.innerHTML = data[index].requesttype;
-      cell5.innerHTML = data[index].requeststatus;
-      cell6.innerHTML = `<button class="btn btn-details" onclick="checkDetails(${data[index].requestid});"> 
-                          <i class="fa fa-trash"></i> Details 
-                        </button>`;
-    }
-  });
+        const cell1 = row.insertCell(0);
+        cell1.setAttribute('data-label', 'Request ID');
+        const cell2 = row.insertCell(1);
+        cell2.setAttribute('data-label', 'User ID');
+        const cell3 = row.insertCell(2);
+        cell3.setAttribute('data-label', 'Product');
+        const cell4 = row.insertCell(3);
+        cell4.setAttribute('data-label', 'Request Type');
+        const cell5 = row.insertCell(4);
+        cell5.setAttribute('data-label', 'Status');
+        const cell6 = row.insertCell(5);
+        cell6.setAttribute('data-label', 'Details');
+
+        cell1.innerHTML = data[index].requestid;
+        cell2.innerHTML = data[index].userid;
+        cell3.innerHTML = data[index].product;
+        cell4.innerHTML = data[index].requesttype;
+        cell5.innerHTML = data[index].requeststatus;
+        cell6.appendChild(detailsBtn);
+
+        detailsBtn.addEventListener('click', () => {
+          fetch(allRequestsUrl + data[index].requestid, options)
+            .then(response => response.json())
+            .then((myData) => {
+              requestDetailsModal(myData.data[0], myData.message);
+            });
+        });
+      }
+    });
+};
+fetchAllRequests();

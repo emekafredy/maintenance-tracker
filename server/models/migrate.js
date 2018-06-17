d35bae3f1b8ce4d3ebadd0f878feb0786d56b2fb
emekafredy <chinedufrderick@gmail.com>
2018-06-18 00:55:52 +0100

    [bg #158344759] Implement code review feedback

--- server/models/migrate.js
+++ server/models/migrate.js
@@ -1,6 +1,21 @@
+import pg from 'pg';
 import winston from 'winston';
-import pool from './database';
 import query from './model';
+
+require('dotenv').config();
+
+const DATABASE_URL = {
+  user: process.env.DB_USERNAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  max: 10,
+  idleTimeoutMillis: 30000,
+};
+
+const pool = new pg.Pool(DATABASE_URL);
+pool.connect();
 
 pool.query(query)
   .then(() => process.exit())

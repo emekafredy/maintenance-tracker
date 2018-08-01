bde9c7fa2a51d3774a35a3a794a9565e4a743520
emekafredy <chinedufrderick@gmail.com>
2018-08-01 12:40:12 +0100

    modify app

--- server/models/seed.js
+++ server/models/seed.js
@@ -17,16 +17,16 @@ const seedUser = `
 const seedRequests = `
   INSERT INTO requests (userId,product,requestDate,requestType,issue,imageUrl) 
   VALUES (2,'laptop','May 29th 2018, 5:11:58 am','repair','It shuts down on its own',
-          'http://res.cloudinary.com/dgbmeqmyf/image/upload/v1528040152/ph.png');
+          'http://res.cloudinary.com/dgbmeqmyf/image/upload/v1528128949/My_Post.jpg');
   INSERT INTO requests (userId,product,requestDate,requestType,issue,imageUrl) 
   VALUES (2,'headphone','May 29th 2018, 5:11:58 am','replace','The speaker is bad',
-          'http://res.cloudinary.com/dgbmeqmyf/image/upload/v1528040152/ph.png');
+          'http://res.cloudinary.com/dgbmeqmyf/image/upload/v1528128949/My_Post.jpg');
   INSERT INTO requests (userId,product,requestDate,requestType,issue,imageUrl) 
   VALUES (2,'monitor','May 29th 2018, 5:11:58 am','repair','Broken screen',
-          'http://res.cloudinary.com/dgbmeqmyf/image/upload/v1528040152/ph.png');
+          'http://res.cloudinary.com/dgbmeqmyf/image/upload/v1528128949/My_Post.jpg');
   INSERT INTO requests (userId,product,requestDate,requestType,issue,imageUrl) 
   VALUES (2,'charger','May 29th 2018, 5:11:58 am','repair','Does not charge my laptop anymore',
-          'http://res.cloudinary.com/dgbmeqmyf/image/upload/v1528040152/ph.png');
+          'http://res.cloudinary.com/dgbmeqmyf/image/upload/v1528128949/My_Post.jpg');
 `;
 const query = `${seedAdmin} ${seedUser} ${seedRequests}`;
 
